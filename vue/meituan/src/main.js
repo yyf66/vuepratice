043b63501860ee46e9e36850b20d22014809a85d
yyf <yyf66@163.com>
2019-01-07 23:32:41 +0800

    creat router folder

--- vue/meituan/src/main.js
+++ vue/meituan/src/main.js
@@ -9,7 +9,8 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+  // router,
+  // components: { App },
+  // template: '<App/>'
+  render: h => h(App)
 })
---
+++ vue/meituan/src/page/person/person.vue
@@ -0,0 +1,21 @@
+<template>
+        <div>
+            person
+        </div>
+</template>
+
+
+
+<script>
+    export default{
+
+    }
+
+</script>
+
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+
+
+</style>
