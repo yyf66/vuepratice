46fb6e3ccd7712e5c84c685b3b818a087bd1f3d0
yyf <yyf66@163.com>
2019-01-08 23:57:40 +0800

    修改了 路由 但是stylus引入还有问题 明天研究下

--- vue/meituan/src/page/order/order.vue
+++ vue/meituan/src/page/order/order.vue
@@ -14,7 +14,7 @@
 </script>
 
 
-<style lang="stylus" rel="stylesheet/stylus">
+<style lang="stylus" >
 
 
 
--- vue/meituan/src/router/index.js
+++ vue/meituan/src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import home from '@/page/home/home'
+import order from '@/page/order/order'
+import person from '@/page/person/person'
+import search from '@/page/search/search'
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/', redirect: '/home' 
+    },{
+      path: '/home',
+      name: 'home',
+      component: home
+    },{
+      path: '/order',
+      name: 'order',
+      component: order
+    },{
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'person',
+      component: person
+    },{
+      path: '/',
+      name: 'search',
+      component: search
+    },
   ]
 })
